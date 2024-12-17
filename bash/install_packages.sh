e9af44bb168820456c686505ba91015f44c12a27
Uizoh <107351887+Uizoh@users.noreply.github.com>
2024-12-17 15:29:15 +0600

    Add install_packages bash script

---
+++ bash/install_packages.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+PKG_MANAGER="dnf"
+FLAGS=""
+ENVTOOLS="gcc g++ clang node make"
+TERMINAL="alacritty"
+LANGS="go zig python lua luajit"
+DEVTOOLS="fzf"
+
+echo $(sudo $PKG_MANAGER install $FLAGS $ENVTOOLS $TERMINAL $CONTAINER $LANGS $DEVTOOLS)
+
