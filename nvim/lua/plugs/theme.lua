98321bd9cd2e45a4a47ec5f234902c1f7a6457dc
Uizoh <107351887+Uizoh@users.noreply.github.com>
2024-12-29 19:47:41 +0600

    Update neovim

--- nvim/lua/plugs/theme.lua
+++ nvim/lua/plugs/theme.lua
@@ -1,1 +1,15 @@
-return { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 }
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        color_overrides = {
+            mocha = {
+                base = "#11121d",
+                mantle = "#1a1b2a",
+                crust = "#212234",
+            },
+        },
+    },
+}
