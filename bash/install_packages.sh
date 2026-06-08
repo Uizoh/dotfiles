097d12b847f5c7464b2e12f56ddc2019fb7a96ba
Uizoh <107351887+Uizoh@users.noreply.github.com>
2026-06-09 03:32:02 +0600

    Edit install script

--- bash/install_packages.sh
+++ bash/install_packages.sh
@@ -4,8 +4,8 @@ PKG_MANAGER="dnf"
 FLAGS=""
 ENVTOOLS="gcc clang node"
 TERMINAL="alacritty"
-LANGS="go zig python lua luajit"
-DEVTOOLS="fzf"
+LANGS="python lua luajit"
+DEVTOOLS="neovim"
 
 echo $(sudo $PKG_MANAGER install $FLAGS $ENVTOOLS $TERMINAL $LANGS $DEVTOOLS)
 
